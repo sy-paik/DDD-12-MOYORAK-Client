ad6eb04a8ad8d621a9c4ed7474857977c69ad2c4
sy-paik <sypaik.dev@gmail.com>
2025-08-02 06:13:24 +0000

    chore: name, email 있을 때 페이지 이동 처리

--- src/pages/AuthPage/RedirectPage.tsx
+++ src/pages/AuthPage/RedirectPage.tsx
@@ -2,14 +2,23 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const RedirectPage = () => {
+	const name = new URLSearchParams(location.search).get('name');
 	const email = new URLSearchParams(location.search).get('email');
+	const profileImage = new URLSearchParams(location.search).get('profileImage');
+
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		if (email) {
-			navigate('/signup');
+		if (name && email) {
+			navigate('/signup', {
+				state: {
+					name: name,
+					email: email,
+					profileImage: profileImage,
+				},
+			});
 		}
-	}, [email]);
+	}, [name, email]);
 
 	return <h1>loading...</h1>;
 };
