192f5512d9b6c931da2f7bfd8df6b307ccdb5032
sy-paik <sypaik.dev@gmail.com>
2025-08-02 03:08:52 +0000

    chore: kakao any 타입으로 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,19 +3,10 @@ export {};
 
 declare global {
 	interface Window {
-		kakao: {
-			maps: {
-				Map: new (container: HTMLElement, options: any) => any;
-				LatLng: new (lat: number, lng: number) => any;
-				Marker: new (options: any) => any;
-				InfoWindow: new (options: any) => any;
-				load: (callback: () => void) => void;
-				event: {
-					addListener: (target: any, type: string, handler: () => void) => void;
-				};
-			};
-		};
+		kakao: any;
 	}
+
+	const kakao: any;
 
 	interface ImportMetaEnv {
 		readonly VITE_KAKAO_MAP_URL: string;
