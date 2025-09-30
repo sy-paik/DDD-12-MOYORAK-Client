179ed9891b5178390ae2a332e480f427694382d7
sy-paik <sypaik.dev@gmail.com>
2025-09-30 11:11:42 +0000

    feat: useMutationMeMealTags에 쿼리 클라이언트 추가 및 성공 시 쿼리 무효화 기능 구현

--- src/apis/useMutationMeMealTags.ts
+++ src/apis/useMutationMeMealTags.ts
@@ -1,6 +1,7 @@
-import { useMutation } from '@tanstack/react-query';
-import { put } from '.';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
 import type { IUserSignUpResponse } from './useMutationAuthSignUp';
+import { put } from '.';
 
 interface IMeMeatlTagsRequest extends IUserSignUpResponse {
 	details: {
@@ -13,8 +14,16 @@ const putMeMealTags = async (request: IMeMeatlTagsRequest) => {
 	return await put('/me/meal/tags', request);
 };
 
-export const useMutationMeMealTags = () =>
+export const useMutationMeMealTags = () => {
+	const queryClient = useQueryClient();
+
 	useMutation<any, Error, IMeMeatlTagsRequest>({
 		mutationKey: ['me', 'meal', 'tags'],
 		mutationFn: putMeMealTags,
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['meal-tags'],
+			});
+		},
 	});
+};
